9592b3fc72fbcd918d7d2f46c532608c0259fa71
Pal Daroczy <daroczypal@outlook.com>
2018-10-14 14:33:59 +0200

    improved outlet style

--- src/components/outlets/TitleAndLogo.tsx
+++ src/components/outlets/TitleAndLogo.tsx
@@ -8,15 +8,11 @@ interface TitleAndLogoProps {
 
 export default (props: TitleAndLogoProps) => (
   <div className="title-grid-container">
+    <div className="logo-container">
+      <img id="ulogo" alt="logo" src={`/assets/media_icons/${props.logo}`} />
+    </div>
     <div className="title-container">
       <div className="controls">
-        <a id="chevron-left" href="#">
-          <img
-            id="chr-left"
-            alt="previous page"
-            src="/assets/baseline-chevron_left-24px.svg"
-          />
-        </a>
         <a id="chevron-right" href="#">
           <img
             id="chr-right"
@@ -24,14 +20,18 @@ export default (props: TitleAndLogoProps) => (
             src="/assets/baseline-chevron_right-24px.svg"
           />
         </a>
+        <a id="chevron-left" href="#">
+          <img
+            id="chr-left"
+            alt="previous page"
+            src="/assets/baseline-chevron_left-24px.svg"
+          />
+        </a>
       </div>
       <div className="title">
         <h2>{props.title}</h2>
         <hr />
       </div>
-    </div>
-    <div className="logo-container">
-      <img id="ulogo" alt="logo" src={`/assets/media_icons/${props.logo}`} />
     </div>
   </div>
 );
