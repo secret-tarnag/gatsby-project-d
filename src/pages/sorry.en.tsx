8ebe92fd5791e33f85d9d73216249fad74a4ac9c
Pal Daroczy <daroczypal@outlook.com>
2018-10-16 20:40:46 +0200

    added sorry page

---
+++ src/pages/sorry.en.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from '../components/layout';
+import './css/sorry.css'
+import { Link } from '@reach/router';
+
+const SorryPage = () => (
+  <Layout>
+    <div id="pic">
+    </div>
+    <div id="sorry">
+      <p id="p1"><b>Dear User!</b><br />Lelkes csapatunk folyamatosan dolgozik a weboldal
+        fejlesztésén, ám az a
+			funkció,<br />melyre klikkeltél, még nem működik. Reméljük, nemsokára kész lesz.</p>
+      <p id="p2">Ha lenne kedved weboldalt fejleszteni vagy közreműködni a projektben, <Link className='sorrylink' to="/en/csatlakozz">csatlakozz</Link>
+        a csapatunkhoz!<br />Democrable Developers</p>
+      <p><Link className='sorrylink' to="/en/">Homepage</Link></p>
+      <p>CreativeCommons 4.0;<br /> 2018, democrable.com</p>
+    </div>
+    <div className="language-button-container">
+      <span className="language-button"><Link className='sorrylink' to='/hu/sorry'>HU</Link></span>
+      <span className="language-button"><Link className='sorrylink' to='/en/sorry'>EN</Link></span>
+    </div>
+  </Layout>
+)
+
+export default SorryPage
