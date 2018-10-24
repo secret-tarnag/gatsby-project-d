32cf5c310b0360ae4fbc08110b7968ad5e6c6ab9
Pal Daroczy <daroczypal@outlook.com>
2018-10-24 12:29:26 +0200

    created propaganda page

---
+++ src/pages/propaganda.en.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Layout from '../components/layout';
+import { graphql } from 'gatsby';
+import { AllOutletsQueryType } from './index.en';
+import Navbar from '../components/Navbar';
+import { css } from 'emotion';
+import Footer from '../components/Footer';
+
+export default ({ data }: AllOutletsQueryType) => (
+  <Layout>
+    <Navbar isHomePage={false} lng="en" urlSlug="propaganda" />
+    <ul
+      className={css({
+        marginTop: 100,
+      })}
+    >
+      {data.outlets.edges
+        .filter(
+          outlet =>
+            outlet.node.frontmatter.lang === 'en' &&
+            !outlet.node.frontmatter.isIndependent
+        )
+        .map(outlet => (
+          <li key={outlet.node.frontmatter.title}>
+            <h2>{outlet.node.frontmatter.title}</h2>
+          </li>
+        ))}
+    </ul>
+    <Footer lng="en" />
+  </Layout>
+);
+
+export const pageQuery = graphql`
+  query PropagPageQueryEn {
+    outlets: allMarkdownRemark(
+      filter: { fileAbsolutePath: { glob: "**/src/pages/outlets/*" } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            lang
+            isIndependent
+          }
+        }
+      }
+    }
+  }
+`;
---
+++ src/pages/propaganda.hu.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Layout from '../components/layout';
+import { graphql } from 'gatsby';
+import { AllOutletsQueryType } from './index.en';
+import Navbar from '../components/Navbar';
+import { css } from 'emotion';
+import Footer from '../components/Footer';
+
+export default ({ data }: AllOutletsQueryType) => (
+  <Layout>
+    <Navbar isHomePage={false} lng="hu" urlSlug="propaganda" />
+    <ul
+      className={css({
+        marginTop: 100,
+      })}
+    >
+      {data.outlets.edges
+        .filter(
+          outlet =>
+            outlet.node.frontmatter.lang === 'hu' &&
+            !outlet.node.frontmatter.isIndependent
+        )
+        .map(outlet => (
+          <li key={outlet.node.frontmatter.title}>
+            <h2>{outlet.node.frontmatter.title}</h2>
+          </li>
+        ))}
+    </ul>
+    <Footer lng="hu" />
+  </Layout>
+);
+
+export const pageQuery = graphql`
+  query PropagPageQueryHu {
+    outlets: allMarkdownRemark(
+      filter: { fileAbsolutePath: { glob: "**/src/pages/outlets/*" } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            lang
+            isIndependent
+          }
+        }
+      }
+    }
+  }
+`;
