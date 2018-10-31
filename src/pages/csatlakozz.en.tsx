ea34bd516caf443deeec36b99fe6058682dbc090
Pal Daroczy <daroczypal@outlook.com>
2018-10-31 22:35:40 +0100

    csatlakozz design done

--- src/pages/csatlakozz.en.tsx
+++ src/pages/csatlakozz.en.tsx
@@ -9,9 +9,9 @@ export default () => (
   <Layout>
     <ShortNavbar lng="en" urlSlug="csatlakozz" />
     <div className="joinus-container">
-      <div className="form-container" role="form">
-        <h2 className="form-title barlow">Applying</h2>
-        <form id="join-us-form" action="csatlakozz.html" method="post">
+      <div className="joinus-form-container barlow" role="form">
+        <h2 className="joinus-form-title">Applying</h2>
+        <form id="joinus-form" action="csatlakozz.html" method="post">
           <div className="tooltip">
             <p id="alias-text">alias*:</p>
             <span id="alias-tooltip" className="tooltiptext">no one will know your real name, please use an ancient greek name</span>
@@ -23,14 +23,14 @@ export default () => (
           </div>
           <input id="email" type="email" name="email" required />
           <p>Which area could you contribute most to?</p>
-          <input id="w1" type="radio" name="worktype" value="webdeveloper" checked /><label for="w1">web development</label>
-          <input id="w2" type="radio" name="worktype" value="creative" /><label for="w2">creative team</label>
-          <input id="w3" type="radio" name="worktype" value="editor" /><label for="w3">content creation</label>
-          <input id="w4" type="radio" name="worktype" value="administrator" /><label for="w4">administration</label>
+          <input id="w1" type="radio" name="worktype" value="webdeveloper" checked /><label for="w1">web development</label><br />
+          <input id="w2" type="radio" name="worktype" value="creative" /><label for="w2">creative team</label><br />
+          <input id="w3" type="radio" name="worktype" value="editor" /><label for="w3">content creation</label><br />
+          <input id="w4" type="radio" name="worktype" value="administrator" /><label for="w4">administration</label><br /><br />
           <input id="accept" type="checkbox" name="acceptedTermsAndConditions" required />
-          <label for="accept"> I have read and agree with the <Link to="docs/adatvedelmi_nyilatkozat.pdf" target="_blank">Data Protection Declaration</Link>*.</label>
+          <label for="accept"> I have read and agree with the <Link to="docs/adatvedelmi_nyilatkozat.pdf" target="_blank">Data Protection Declaration</Link>*.</label><br /><br />
           <input type="reset" value="clear" />
-          <input id="submit" type="submit" value="send" disabled />
+          <input id="submit" type="submit" value="send" />
         </form>
       </div>
     </div>
--- src/pages/csatlakozz.hu.tsx
+++ src/pages/csatlakozz.hu.tsx
@@ -9,9 +9,9 @@ export default () => (
   <Layout>
     <ShortNavbar lng="hu" urlSlug="csatlakozz" />
     <div className="joinus-container">
-      <div className="form-container" role="form">
-        <h2 className="form-title barlow">Jelentkezés</h2>
-        <form id="join-us-form" action="csatlakozz.html" method="post">
+      <div className="joinus-form-container barlow" role="form">
+        <h2 className="joinus-form-title">Jelentkezés</h2>
+        <form className="joinus-form" action="csatlakozz.html" method="post">
           <div className="tooltip">
             <p id="alias-text">álnév*:</p>
             <span id="alias-tooltip" className="tooltiptext">az igazit nem tudja meg senki, ellenben ez nyilvános; magasabb
@@ -25,14 +25,14 @@ export default () => (
           </div>
           <input id="email" type="email" name="email" required />
           <p>melyik területen szeretnél közreműködni?</p>
-          <input id="w1" type="radio" name="worktype" value="webdeveloper" checked /><label for="w1">webfejlesztés</label>
-          <input id="w2" type="radio" name="worktype" value="creative" /><label for="w2">kreatív csapat</label>
-          <input id="w3" type="radio" name="worktype" value="editor" /><label for="w3">tartalomfejlesztés</label>
-          <input id="w4" type="radio" name="worktype" value="administrator" /><label for="w4">adminisztráció</label>
+          <input id="w1" type="radio" name="worktype" value="webdeveloper" checked /><label for="w1">webfejlesztés</label><br />
+          <input id="w2" type="radio" name="worktype" value="creative" /><label for="w2">kreatív csapat</label><br />
+          <input id="w3" type="radio" name="worktype" value="editor" /><label for="w3">tartalomfejlesztés</label><br />
+          <input id="w4" type="radio" name="worktype" value="administrator" /><label for="w4">adminisztráció</label><br /><br />
           <input id="accept" type="checkbox" name="acceptedTermsAndConditions" required />
-          <label for="accept"> elolvastam és elfogadom az <Link to="docs/adatvedelmi_nyilatkozat.pdf" target="_blank">Adatvédelmi Nyilatkozatot</Link>*.</label>
+          <label for="accept"> elolvastam és elfogadom az <Link to="docs/adatvedelmi_nyilatkozat.pdf" target="_blank">Adatvédelmi Nyilatkozatot</Link>*.</label><br /><br />
           <input type="reset" value="törlés" />
-          <input id="submit" type="submit" value="küldés" disabled />
+          <input id="submit" type="submit" value="küldés" />
         </form>
       </div>
     </div>
