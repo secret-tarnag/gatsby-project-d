a40abd638f369d2c568169137cf029e91bc4fc37
Pal Daroczy <daroczypal@outlook.com>
2018-11-07 17:01:28 +0100

    finished modal form

--- src/components/aboutpage/SendMessage.tsx
+++ src/components/aboutpage/SendMessage.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import * as Modal from 'react-modal'
+import { css } from 'emotion';
 
-interface SendMessageProps { };
+interface SendMessageProps {
+  lng: 'en' | 'hu'
+};
 interface SendMessageState {
   showModal: boolean
 };
@@ -13,29 +16,125 @@ const SendMessageStyles = {
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(255, 255, 255, 0.75)'
+    backgroundColor: 'rgba(255, 255, 255, 0.2)'
   },
   content: {
     position: 'absolute',
-    top: '40px',
-    left: '40px',
-    right: '40px',
-    bottom: '40px',
-    border: '1px solid #ccc',
+    top: 100,
+    left: 10,
+    right: 10,
+    bottom: 16,
+    border: 'none',
     background: '#fff',
     overflow: 'auto',
     WebkitOverflowScrolling: 'touch',
-    borderRadius: '4px',
+    borderRadius: '0px',
     outline: 'none',
-    padding: '20px'
+    padding: '0px'
   }
 }
 
-Modal.setAppElement(document.getElementById('modal-root'));
+const openButtonContainer = css({
+  width: '15rem',
+  margin: '1rem auto',
+});
+const openButtonStyles = css({
+  border: '1px solid black',
+  /*border-radius: 2px;*/
+  fontSize: '1.5rem',
+  padding: '0.5rem',
+  color: 'white',
+  backgroundColor: 'black',
+  transition: 'all .2s',
+  ':hover': {
+    color: 'black',
+    backgroundColor: 'white',
+  }
+});
+const closeButtonStyles = css({
+  position: 'relative',
+  float: 'right',
+  margin: '2rem',
+  fontSize: '1.5rem',
+  fontWeight: 'bold',
+  color: 'white',
+  bottom: '5rem',
+  cursor: 'pointer',
+  clear: 'both',
+});
+const formContainer = css({
+  width: '100%',
+  background: '#4b6677',
+  padding: '2rem 0',
+});
+const messageForm = css({
+  width: '70%',
+  position: 'relative',
+  left: '50%',
+  transform: 'translateX(-50%)',
+});
+const formTitle = css({
+  margin: '1rem',
+  fontSize: '2rem',
+  color: '#fff',
+  textAlign: 'center',
+});
+const formText = css({
+  margin: '0.5rem 0',
+  color: 'white',
+  padding: '0.5rem 0',
+  fontSize: '1.2rem',
+  display: 'block',
+});
+const textarea = css({
+  width: '96%',
+  height: '6rem',
+  resize: 'none',
+  backgroundColor: '#eee',
+  border: '0.125rem solid transparent',
+  fontSize: '1.2rem',
+  padding: '0.2rem 1rem',
+  ':hover': {
+    backgroundColor: '#ddd',
+  },
+  ':focus': {
+    backgroundColor: '#ddd',
+    border: '0.125rem solid #727272',
+  },
+});
+
+const texts = {
+  openButton: {
+    en: 'direct message',
+    hu: 'direkt üzenetet küldök',
+  },
+  title: {
+    en: 'You can send us a direct message through this form:',
+    hu: 'Üzenj nekünk közvetlenül innen:',
+  },
+  name: {
+    en: 'Name:',
+    hu: 'Név:',
+  },
+  email: {
+    en: 'Email:',
+    hu: 'E-mail:',
+  },
+  subject: {
+    en: 'Subject:',
+    hu: 'Tárgy:',
+  },
+  message: {
+    en: 'Message:',
+    hu: 'Üzenet:',
+  }
+}
+
+Modal.setAppElement(document.getElementById('about-modal-root'));
 
 class SendMessage extends React.PureComponent<SendMessageProps, SendMessageState> {
-  constructor() {
-    super('');
+  constructor(props: SendMessageProps) {
+    super(props);
     this.state = {
       showModal: false
     };
@@ -54,80 +153,73 @@ class SendMessage extends React.PureComponent<SendMessageProps, SendMessageState
   render() {
     return (
       <div>
-        <div id="get-mbb-container">
+        <div className={openButtonContainer}>
           <button
-            id="get-messagebox-button"
-            className="barlow"
+            className={`barlow ${openButtonStyles}`}
             onClick={this.handleOpenModal}
           >
-            direkt üzenetet küldök
-        </button>
+            {texts.openButton[this.props.lng]}
+          </button>
         </div>
-        <Modal isOpen={this.state.showModal} onRequestClose={this.handleCloseModal}>
-          <div className="contact-form-container" id="contact-form">
-            <div className="title-container">
-              <h3 id="contact-form-title" className="barlow">
-                Üzenj nekünk közvetlenül innen:
-              </h3>
-            </div>
-            <div className="label-container">
-              <p id="name-label" className="barlow form-text">
-                Név:
-              </p>
-              <p id="email-label" className="barlow form-text">
-                E-mail:
-              </p>
-              <p id="subject-label" className="barlow form-text">
-                Tárgy:
-              </p>
-              <p id="message-label" className="barlow form-text">
-                Üzenet:
-              </p>
-            </div>
-            <div className="input-container" role="form">
-              <span className="close">&times;</span>
-              <form id="form" action="rolunk.html" method="post">
-                <input
-                  id="name"
-                  className="barlow form-field"
-                  type="text"
-                  name="name"
-                  required
-                />
-                <input
-                  id="email"
-                  className="barlow form-field"
-                  type="email"
-                  name="email"
-                  required
-                />
-                <input
-                  id="subject"
-                  className="barlow form-field"
-                  type="text"
-                  name="subject"
-                  required
-                />
-                <br />
-                <textarea
-                  id="message"
-                  className="barlow form-field"
-                  name="message"
-                  rows={1}
-                  cols={100}
-                  required
-                />
-                <input
-                  id="submit-button"
-                  className="barlow"
-                  type="submit"
-                  value="küldés"
-                />
-              </form>
-            </div>
+        {/*out: className:contact-form-container id:contact-form*/}
+        <Modal isOpen={this.state.showModal} onRequestClose={this.handleCloseModal} style={SendMessageStyles}>
+          <div className={formContainer}>
+            <h3 className={`barlow ${formTitle}`}>
+              {texts.title[this.props.lng]}
+            </h3>
+            <span className={closeButtonStyles} onClick={this.handleCloseModal}>&times;</span>
+            <form id="form" action="" method="post" className={messageForm}>
+              <span className={`barlow ${formText}`}>
+                {texts.name[this.props.lng]}
+              </span>
+              <input
+                id="name"
+                className="barlow"
+                type="text"
+                name="name"
+                required
+              />
+              <span className={`barlow ${formText}`}>
+                {texts.email[this.props.lng]}
+              </span>
+              <input
+                id="email"
+                className="barlow"
+                type="email"
+                name="email"
+                required
+              />
+              <span className={`barlow ${formText}`}>
+                {texts.subject[this.props.lng]}
+              </span>
+              <input
+                id="subject"
+                className="barlow"
+                type="text"
+                name="subject"
+                required
+              />
+              <span className={`barlow ${formText}`}>
+                {texts.message[this.props.lng]}
+              </span>
+              <textarea
+                id="message"
+                className={`barlow ${textarea}`}
+                name="message"
+                rows={1}
+                cols={100}
+                required
+              /><br /><br />
+              <input
+                id="submit-button"
+                className="barlow"
+                type="submit"
+                value="küldés"
+              />
+            </form>
           </div>
         </Modal>
-      </div>
+      </div >
     );
   }
 
--- src/pages/rolunk.hu.tsx
+++ src/pages/rolunk.hu.tsx
@@ -8,162 +8,161 @@ import SendMessage from '../components/aboutpage/SendMessage';
 
 export default () => (
   <Layout>
-    <Navbar id="" lng="hu" urlSlug="rolunk" isHomePage={false} />
-    <div className="about_main barlow" id="heading">
-      <h2 id="heading-title">Rólunk</h2>
-    </div>
-    <div className="barlow local-nav">
-      <span>
-        <a href="#introduction">bemutatkozás</a>
-      </span>
-      <span>
-        <a href="#contribution">önkéntesség</a>
-      </span>
-      <span>
-        <a href="#privacy">adatvédelem</a>
-      </span>
-      <span>
-        <a href="#contactinfo">elérhetőségek</a>
-      </span>
-      <span>
-        <a href="#contributors">impresszum</a>
-      </span>
-    </div>
-    <div className="general-text" id="introduction">
-      <h2>Bemutatkozás</h2>
-      <br />
-      <span className="emphasize">Kik vagyunk mi?</span>
-      <br />
-      <p>
-        Pár fiatal, egyetemista, akiknek elegük van. Húha, ez nagyon harciasra
-        sikerült. Azért nem kell megijedni tőlünk, nem terveztünk zavarogni vagy
-        ilyesmi. Magyarországon amúgy sem szoktak sikerülni az elnyomó csoportok
-        elleni forradalmak (lásd a korábbi példákat).
-      </p>
-      <br />
-      <br />
-      <span className="emphasize">Mit szeretnénk?</span>
-      <br />
-      <p>
-        Szabad médiát!
+    <div id="about-modal-root">
+      <Navbar id="" lng="hu" urlSlug="rolunk" isHomePage={false} />
+      <div className="about_main barlow" id="heading">
+        <h2 className="about-heading">Rólunk</h2>
+      </div>
+      <div className="barlow local-nav">
+        <span>
+          <a href="#introduction">bemutatkozás</a>
+        </span>
+        <span>
+          <a href="#contribution">önkéntesség</a>
+        </span>
+        <span>
+          <a href="#privacy">adatvédelem</a>
+        </span>
+        <span>
+          <a href="#contactinfo">elérhetőségek</a>
+        </span>
+        <span>
+          <a href="#contributors">impresszum</a>
+        </span>
+      </div>
+      <div className="general-text" id="introduction">
+        <h2>Bemutatkozás</h2>
         <br />
-        Valaki mondta, hogy a választás nem lehet szabad, ha az emberek
-         nem dönthetnek szabadon. A szabad döntéshez azonban szükség van arra,
-         hogy a média ne befolyásolja az embereket, hanem hitelesen, tényszerűen
-         tájékoztassa. Nyilván nem lehet teljesen semlegesnek lenni - az akkor
-         lenne, ha nem is írnának egy szót sem - de el lehet érni azt a szintet,
-         amit független és objektív újságírásnak lehet nevezni.
+        <span className="emphasize">Kik vagyunk mi?</span>
+        <br />
+        <p>
+          Pár fiatal, egyetemista, akiknek elegük van. Húha, ez nagyon harciasra
+          sikerült. Azért nem kell megijedni tőlünk, nem terveztünk zavarogni vagy
+          ilyesmi. Magyarországon amúgy sem szoktak sikerülni az elnyomó csoportok
+          elleni forradalmak (lásd a korábbi példákat).
       </p>
-      <br />
-      <br />
-    </div>
-    <div className="general-text" id="contribution">
-      <h2>Önkéntesség</h2>
-      <br />
-      <p>
-        Ha úgy érzed, segítenél a magyar demokráciának az maradni, aminek kell,
-        és ebben a honlapban (szervezetben?) láttad meg a fantáziát vagy a
-        tökéletes eszközt célod eléréséhez, több lehetőség közül választhatsz,
-        hogy támogass minket (mindegyiket hálás szívvel fogadjuk).
+        <br />
+        <br />
+        <span className="emphasize">Mit szeretnénk?</span>
+        <br />
+        <p>
+          Szabad médiát!
+        <br />
+          Valaki mondta, hogy a választás nem lehet szabad, ha az emberek
+           nem dönthetnek szabadon. A szabad döntéshez azonban szükség van arra,
+           hogy a média ne befolyásolja az embereket, hanem hitelesen, tényszerűen
+           tájékoztassa. Nyilván nem lehet teljesen semlegesnek lenni - az akkor
+           lenne, ha nem is írnának egy szót sem - de el lehet érni azt a szintet,
+           amit független és objektív újságírásnak lehet nevezni.
       </p>
-      <ul className="indention">
-        <li>Írd meg a véleményed az oldalról és küldd el nekünk.</li>
-        <li>
-          Ha elavult, téves vagy hiányzó információt találsz írd meg az{' '}
-          <a className="about-links" href="#contactinfo">
-            alábbi módok
+        <br />
+        <br />
+      </div>
+      <div className="general-text" id="contribution">
+        <h2>Önkéntesség</h2>
+        <br />
+        <p>
+          Ha úgy érzed, segítenél a magyar demokráciának az maradni, aminek kell,
+          és ebben a honlapban (szervezetben?) láttad meg a fantáziát vagy a
+          tökéletes eszközt célod eléréséhez, több lehetőség közül választhatsz,
+          hogy támogass minket (mindegyiket hálás szívvel fogadjuk).
+      </p>
+        <ul className="indention">
+          <li>Írd meg a véleményed az oldalról és küldd el nekünk.</li>
+          <li>
+            Ha elavult, téves vagy hiányzó információt találsz írd meg az{' '}
+            <a className="about-links" href="#contactinfo">
+              alábbi módok
           </a>{' '}
-          egyikén, igyekszünk frissíteni, javítani, kiegészíteni.
+            egyikén, igyekszünk frissíteni, javítani, kiegészíteni.
         </li>
-        <li>
-          Ha programozó lennél, akkor ahogy az angol mondja:{' '}
+          <li>
+            Ha programozó lennél, akkor ahogy az angol mondja:{' '}
+            <a
+              className="about-links"
+              href="https://github.com/daroczypal/project-d-."
+              target="_blank"
+            >
+              Fork us on GitHub
+          </a>
+            .
+        </li>
+          <li>
+            Kedved lenne az első vonalból szemlélni, sőt alakítani a dolgokat?{' '}
+            <Link className="about-links" to="/hu/csatlakozz">
+              Csatlakozz
+          </Link>{' '}
+            hozzánk!
+        </li>
+        </ul>
+      </div>
+      <div className="general-text" id="privacy">
+        <h2>Adatvédelem</h2>
+        <br />
+        <p>
+          Kérünk titeket, olvassátok el az{' '}
+          <a href="/hu/adatvedelem" target="_blank">
+            Adatvédelmi Nyilatkozatot
+        </a>
+          .
+      </p>
+        <h4>Forráskód</h4>
+        <p>
+          Egyébként a weboldal{' '}
           <a
             className="about-links"
             href="https://github.com/daroczypal/project-d-."
             target="_blank"
           >
-            Fork us on GitHub
-          </a>
-          .
-        </li>
-        <li>
-          Kedved lenne az első vonalból szemlélni, sőt alakítani a dolgokat?{' '}
-          <Link className="about-links" to="/hu/csatlakozz">
-            Csatlakozz
-          </Link>{' '}
-          hozzánk!
-        </li>
-      </ul>
-    </div>
-    <div className="general-text" id="privacy">
-      <h2>Adatvédelem</h2>
-      <br />
-      <p>
-        Kérünk titeket, olvassátok el az{' '}
-        <a href="/hu/adatvedelem" target="_blank">
-          Adatvédelmi Nyilatkozatot
+            forráskódját
         </a>
-        .
+          bárki ellenőrizheti a GitHubon.
       </p>
-      <h4>Forráskód</h4>
-      <p>
-        Egyébként a weboldal{' '}
-        <a
-          className="about-links"
-          href="https://github.com/daroczypal/project-d-."
-          target="_blank"
-        >
-          forráskódját
-        </a>
-        bárki ellenőrizheti a GitHubon.
-      </p>
-    </div>
-    <div className="general-text" id="contactinfo">
-      <h2>Elérhetőségeink</h2>
-      <p>
-        E-mail: <a href="mailto:democrable@gmail.com">democrable@gmail.com</a>
-        <br />
-        Ezt biztosan elolvassuk, és válaszolunk. De ha valaki nem szeretne ímélt
-        küldeni, akkor megfontolhatja, hogy az alábbi formmal küld nekünk
-        üzenetet vagy valamelyik közösségi oldalon:
-      </p>
-      <span className="rolunk-sm-span">
-        <SocialMediaLinks />
-      </span>
-      {/*Modal comes here */}
-      <div id="modal-root">
-        <SendMessage />
       </div>
-    </div>
-    <div id="message-sent">
-      <span className="barlow">Az üzenet sikeresen elküldve.</span>
-      <span className="close">&times;</span>
-    </div>
-    <div id="message-error">
-      <span className="barlow">Hiba történt. Az üzenet nem lett elküldve.</span>
-      <span className="close">&times;</span>
-    </div>
-    <div className="contributors" id="contributors">
-      <div id="left" className="grid-item barlow">
-        <div className="grid-item-center">
-          <h3 className="bold inline">Impresszum</h3>
-          <p className="inline">&nbsp;- közreműködők, írók, fejlesztők:</p>
-          <p className="indention">webfejlesztés: Démokritosz, Hermész</p>
-          <p className="indention">tartalom, dizájn: Lakedaimón</p>
-          <p className="indention">fotó: </p>
-          <p className="license">
-            2017-2018;{' '}
-            <img
-              className="cc"
-              id="cc-about"
-              alt="Creative Commons"
-              src="/assets/cc.svg"
-            />{' '}
-            by-nc-sa 4.0
+      <div className="general-text" id="contactinfo">
+        <h2>Elérhetőségeink</h2>
+        <p>
+          E-mail: <a href="mailto:democrable@gmail.com">democrable@gmail.com</a>
+          <br />
+          Ezt biztosan elolvassuk, és válaszolunk. De ha valaki nem szeretne ímélt
+          küldeni, akkor megfontolhatja, hogy az alábbi formmal küld nekünk
+          üzenetet vagy valamelyik közösségi oldalon:
+      </p>
+        <span className="rolunk-sm-span">
+          <SocialMediaLinks />
+        </span>
+        <SendMessage lng="hu" />
+      </div>
+      <div id="message-sent">
+        <span className="barlow">Az üzenet sikeresen elküldve.</span>
+        <span className="close">&times;</span>
+      </div>
+      <div id="message-error">
+        <span className="barlow">Hiba történt. Az üzenet nem lett elküldve.</span>
+        <span className="close">&times;</span>
+      </div>
+      <div className="contributors" id="contributors">
+        <div id="left" className="grid-item barlow">
+          <div className="grid-item-center">
+            <h3 className="bold inline">Impresszum</h3>
+            <p className="inline">&nbsp;- közreműködők, írók, fejlesztők:</p>
+            <p className="indention">webfejlesztés: Démokritosz, Hermész</p>
+            <p className="indention">tartalom, dizájn: Lakedaimón</p>
+            <p className="indention">fotó: </p>
+            <p className="license">
+              2017-2018;{' '}
+              <img
+                className="cc"
+                id="cc-about"
+                alt="Creative Commons"
+                src="/assets/cc.svg"
+              />{' '}
+              by-nc-sa 4.0
           </p>
+          </div>
         </div>
+        <div id="right" className="grid-item icon-container" />
       </div>
-      <div id="right" className="grid-item icon-container" />
     </div>
   </Layout>
 );
