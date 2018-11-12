85c63383aa157b3280f48b745c11c291b8e3b6a1
Pal Daroczy <daroczypal@outlook.com>
2018-11-12 14:35:37 +0100

    inline css for TabNav

--- src/components/donate/TabNav.tsx
+++ src/components/donate/TabNav.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { css } from 'emotion';
 
 interface TabNavProps {
   lng: 'en' | 'hu'
@@ -20,7 +21,46 @@ const links = [
     hu: 'Segíts nekünk!',
     id: 'contribute',
   }
-]
+];
+
+const tabImage = css({
+  width: '100%',
+  height: '8rem',
+  background: 'url(/assets/shapes_bckg.png) no-repeat right',
+  backgroundSize: 'contain',
+});
+
+const tabStyle = css({
+  margin: '0',
+  height: '10rem',
+  backgroundColor: 'white',
+  overflow: 'hidden',
+  display: 'grid',
+  gridTemplateColumns: '20% 25% 25% 25%',
+  gridTemplateRows: 'auto',
+  gridGap: '3rem',
+  alignContent: 'center',
+  justifyContent: 'flex-end',
+  position: 'sticky',
+  top: '0rem',
+  zIndex: 5,
+  '& button': {
+    fontFamily: '"Roboto Condensed", sans-serif',
+    fontSize: '1.1rem',
+    backgroundColor: 'inherit',
+    width: '80%',
+    height: '8rem',
+    border: 'none',
+    outline: 'none',
+    cursor: 'pointer',
+    padding: '0',
+    transition: '0.3s',
+  },
+  '& button:hover': {
+    textDecoration: 'underline',
+  },
+});
+//media query
 
 export default class TabNav extends React.PureComponent<TabNavProps, ''>
 {
@@ -28,13 +68,14 @@ export default class TabNav extends React.PureComponent<TabNavProps, ''>
     super(props);
   }
 
-  openTab(tabName) {
+  openTab(tabName: string) {
     var i;
     var tablinks = document.getElementsByClassName('tablinks');
-
+    //remove past active class selectors
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].classList.remove('active')
     }
+    //scrolling
     var topPos = document.getElementById(tabName).offsetTop;
     document.documentElement.scrollTop = topPos - 160;
     // add an "active" class to the button that opened the tab
@@ -43,8 +84,8 @@ export default class TabNav extends React.PureComponent<TabNavProps, ''>
 
   render() {
     return (
-      <div id="tab" className="tab">
-        <div id="image2" className="tab-image-container"></div>
+      <div id="tab" className={tabStyle}>
+        <div id="tab-image" className={tabImage}></div>
         {
           links.map(link => (
             <div>
