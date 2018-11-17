e75e4e1fc658e4e69a50dcc567204f2d094db1dd
Pal Daroczy <daroczypal@outlook.com>
2018-11-17 12:08:43 +0100

    scrolltop button

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -1,12 +1,39 @@
 import * as React from 'react';
+import { css } from 'emotion';
+
+const buttonContainer = css({
+  display: 'none',
+  position: 'fixed',
+  top: '7rem',
+  right: '5rem',
+  zIndex: 4,
+});
+const button = css({
+  height: '2rem',
+  width: '2rem',
+  border: '1px solid #969696',
+  borderRadius: '50%',
+  outline: 'none',
+  backgroundColor: 'transparent',
+  color: '#000',
+  ':hover': {
+    borderColor: '#000',
+  },
+  '@media(max-width:1000px)': {
+    right: '0.5rem',
+  },
+  '@media(max-width: 600px)': {
+    right: '-3rem',
+  }
+});
 
 export default () => (
-  <div className="scrolltop-button-container" id="st-butt-cont">
+  <div className={buttonContainer} id="st-butt-cont">
     <button
       onClick={() => {
         document.documentElement.scrollTop = 0;
       }}
-      className="scrolltop-button"
+      className={button}
     >
       <i className="material-icons">expand_less</i>
     </button>
