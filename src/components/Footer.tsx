d15a62db93be42b209c07a356c70331609efe18f
Pal Daroczy <daroczypal@outlook.com>
2018-11-17 11:59:29 +0100

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Link from 'gatsby-link';
 import SocialMediaLinks from './SocialMediaLinks';
+import { css, cx } from 'emotion';
 
 interface FooterProps {
   lng: 'en' | 'hu';
@@ -61,26 +62,91 @@ const fourthRow = [
   },
 ];
 
+const footerStyle = css({
+  width: '100%',
+  height: '10rem',
+  marginTop: '2rem',
+  background: 'linear-gradient(to right, rgba(255, 188, 4, 0.76), rgba(255, 177, 117, 0.54))',
+  '@media(max-width:600px)': {
+    height: '35rem',
+  }
+});
+const footerGrid = css({
+  display: 'grid',
+  gridTemplateColumns: 'auto auto auto auto',
+  gridTemplateRows: '2rem 1.3rem',
+  gridAutoFlow: 'row',
+  gridAutoRows: '1.3rem',
+  gridGap: '0.6rem',
+  justifyContent: 'space-around',
+  '@media(max-width: 600px)': {
+    gridTemplateColumns: 'auto',
+    gridTemplateRows: '3rem 3rem 3rem 3rem',
+    gridAutoRows: '2rem',
+    textAlign: 'center',
+  }
+});
+const gridItem = css({
+  display: 'inline',
+  fontFamily: '"Barlow Semi Condensed", sans-serif',
+  fontSize: '1rem',
+  textAlign: 'center',
+  '& a': {
+    color: 'black',
+    backgroundColor: 'transparent',
+    textDecoration: 'none',
+  },
+  '& a:hover': {
+    color: 'black',
+    backgroundColor: 'transparent',
+    textDecoration: 'underline solid',
+  },
+  '& a:active': {
+    color: 'black',
+    backgroundColor: 'lightgray',
+    textDecoration: 'none',
+  },
+  '@media(max-width:600px)': {
+    fontSize: '1.3rem',
+  }
+});
+const firstRowStyle = css({
+  fontSize: '1.2rem',
+  fontWeight: 'bold',
+  marginTop: '0.6rem',
+  '@media(max-width:600px)': {
+    fontSize: '1.8rem',
+  }
+});
+const footerEmailLink = css({
+  color: 'black',
+});
+const footerBlank = css({
+  '@media(max-width: 600px)': {
+    display: 'none',
+  }
+});
+
 export default (props: FooterProps) => (
   <footer
     id={props.id ? props.id : ''}
-    className="general-footer"
+    className={footerStyle}
     role="contentinfo"
   >
     <hr />
-    <div className="footer-grid-container">
+    <div className={footerGrid}>
       {firstRow.map(item => (
-        <div className="footer-grid-item first-row" key={item.hu}>
+        <div className={cx(gridItem, firstRowStyle)} key={item.hu}>
           <Link to={`${props.lng}/${item.link}`}>{item[props.lng]}</Link>
         </div>
       ))}
-      <div className="footer-grid-item first-row">2017-2018; Democrable</div>
+      <div className={cx(gridItem, firstRowStyle)}>2017-2018; Democrable</div>
       {secondRow.map(item => (
-        <div className="footer-grid-item" key={item.hu}>
+        <div className={gridItem} key={item.hu}>
           <Link to={`${props.lng}/${item.link}`}>{item[props.lng]}</Link>
         </div>
       ))}
-      <div className="footer-grid-item" id="license">
+      <div className={gridItem} id="license">
         <a
           rel="license"
           href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
@@ -97,30 +163,30 @@ export default (props: FooterProps) => (
         />
       </div>
       {thirdRow.map(item => (
-        <div className="footer-grid-item" key={item.hu}>
+        <div className={gridItem} key={item.hu}>
           <Link to={`${props.lng}/${item.link}`}>{item[props.lng]}</Link>
         </div>
       ))}
-      <div className="footer-grid-item footer-blank">
+      <div className={cx(gridItem, footerBlank)}>
         <Link to="/sorry" />
       </div>
-      <div className="footer-grid-item" id="footer-email">
-        <a href="mailto:democrable@gmail.com" id="footer-email-link">
+      <div className={gridItem} id="footer-email">
+        <a href="mailto:democrable@gmail.com" className={footerEmailLink}>
           democrable@gmail.com
         </a>
       </div>
       {fourthRow.map(item => (
-        <div className="footer-grid-item" key={item.hu}>
+        <div className={gridItem} key={item.hu}>
           <Link to={`${props.lng}/${item.link}`}>{item[props.lng]}</Link>
         </div>
       ))}
-      <div className="footer-grid-item footer-blank">
+      <div className={cx(gridItem, footerBlank)}>
         <Link to="/sorry" />
       </div>
-      <div className="footer-grid-item footer-blank">
+      <div className={cx(gridItem, footerBlank)}>
         <Link to="/sorry" />
       </div>
-      <div className="footer-grid-item">
+      <div className={gridItem}>
         <SocialMediaLinks />
       </div>
     </div>
