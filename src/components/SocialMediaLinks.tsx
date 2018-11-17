0013e7f079dcbd4b39bde6146a3cf641822917d5
Pal Daroczy <daroczypal@outlook.com>
2018-11-17 12:18:05 +0100

    socialmedia icons

--- src/components/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks.tsx
@@ -1,33 +1,47 @@
 import * as React from 'react';
-import Link from 'gatsby-link';
+import { css, cx } from 'emotion';
+
+const icon = css({
+  width: '2.6rem',
+  height: '2.6rem',
+  margin: '0 0.3rem',
+  ':hover': {
+    opacity: 0.85,
+  },
+});
+const github = css({
+  width: '2rem',
+  height: '2rem',
+  position: 'relative',
+  bottom: '0.4rem',
+});
 
 export default () => (
   <div>
     <a href="https://www.facebook.com/Democrable" target="_blank">
       <img
-        className="social-media-icon"
+        className={icon}
         alt="Facebook logo"
         src="/assets/if_facebook_2136487.svg"
       />
     </a>
     <a href="https://twitter.com/democrable" target="_blank">
       <img
-        className="social-media-icon"
+        className={icon}
         alt="Twitter logo"
         src="/assets/if_twitter_2136496.svg"
       />
     </a>
     <a href="https://www.youtube.com/channel/UCL55wcXTkwrUZvdoK5HUazQ?view_as=subscriber" target="_blank">
       <img
-        className="social-media-icon"
+        className={icon}
         alt="Youtube logo"
         src="/assets/if_youtube-social-media_765195.svg"
       />
     </a>
     <a href="https://github.com/daroczypal/project-d-." target="_blank">
       <img
-        id="github"
-        className="social-media-icon"
+        className={cx(icon, github)}
         alt="Github logo"
         src="/assets/if_icon-social-github_211904.svg"
       />
