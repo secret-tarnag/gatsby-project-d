801be2f12941a819eeacb80a470a07d24c50a6cb
Pal Daroczy <daroczypal@outlook.com>
2019-01-06 17:30:25 +0100

    linting

--- src/components/outlets/Sources.tsx
+++ src/components/outlets/Sources.tsx
@@ -6,8 +6,8 @@ interface SourcesProps {
 }
 
 const text = {
-  'en': 'Sources, documents',
-  'hu': 'Források, dokumentumok',
+  en: 'Sources, documents',
+  hu: 'Források, dokumentumok',
 };
 
 export default (props: SourcesProps) => (
