5f5f62448f1734c1feb06e9b643e0d55dee6e60a
Pal Daroczy <daroczypal@outlook.com>
2019-01-10 16:50:29 +0100

    inline style for Description

--- src/components/outlets/Description.tsx
+++ src/components/outlets/Description.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
+import { css } from 'emotion';
 
 interface DescriptionProps {
   description: Array<{ text: string; }>;
 }
 
+const descriptionContainer = css({
+  width: '90%',
+  margin: '1rem auto',
+  fontSize: '1.15rem',
+});
+const descriptionParagraphs = css({
+  margin: '0.3rem 0',
+});
+
 export default (props: DescriptionProps) => (
-  <div>
+  <div className={descriptionContainer}>
     {props.description.map(paragraph => (
-      <p>{paragraph.text}</p>
+      <p className={descriptionParagraphs}>{paragraph.text}</p>
     ))}
   </div>
 );
